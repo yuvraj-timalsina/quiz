615f4b417d924ef286e8d272845fe0e3cb9cb15f
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-04-22 21:30:41 +0545

    Questions CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,35 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\ProfileController;
+    use App\Http\Livewire\Questions\QuestionForm;
+    use App\Http\Livewire\Questions\QuestionList;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "web" middleware group. Make something great!
+    |
+    */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::view('/', 'welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::view('/dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-require __DIR__.'/auth.php';
+        Route::middleware('isAdmin')->group(function () {
+            Route::get('questions', QuestionList::class)->name('questions');
+            Route::get('questions/create', QuestionForm::class)->name('questions.create');
+            Route::get('questions/{question}', QuestionForm::class)->name('questions.edit');
+        });
+    });
+
+    require __DIR__ . '/auth.php';
