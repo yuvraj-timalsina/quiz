d4aae358ab89c1352a65dfa3fde78ada7c270dce
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-04-23 13:28:11 +0545

    My Results

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@
 
 
     Route::middleware('auth')->group(function () {
+        Route::get('results', [ResultController::class, 'index'])->name('results.index');
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
