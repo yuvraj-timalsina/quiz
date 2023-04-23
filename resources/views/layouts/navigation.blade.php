7e63a4dcef9bfcb98aa8ca5e9bf6b7870cbd4896
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-04-23 12:34:26 +0545

    Quiz for guest

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,16 +5,15 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <a href="{{ route('home') }}">
+
+                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+
                 </div>
             </div>
 
@@ -45,6 +44,7 @@
                     </x-slot>
                 </x-dropdown>
                 @endadmin
+                @auth
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
@@ -75,6 +75,10 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+                @else
+                    <x-nav-link :href="route('login')">Log in</x-nav-link>
+                    <x-nav-link :href="route('register')">Register</x-nav-link>
+                @endauth
             </div>
 
             <!-- Hamburger -->
@@ -92,13 +96,12 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+
         </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
+            @auth
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
@@ -120,6 +123,10 @@
                     </x-responsive-nav-link>
                 </form>
             </div>
+            @else
+                <x-responsive-nav-link :href="route('login')">Log in</x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('register')">Register</x-responsive-nav-link>
+            @endauth
         </div>
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+    use App\Http\Controllers\HomeController;
     use App\Http\Controllers\ProfileController;
     use App\Http\Livewire\Questions\QuestionForm;
     use App\Http\Livewire\Questions\QuestionList;
@@ -7,9 +8,8 @@
     use App\Http\Livewire\Quiz\QuizList;
     use Illuminate\Support\Facades\Route;
 
-    Route::view('/', 'welcome');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-    Route::view('/dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
     Route::middleware('auth')->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
